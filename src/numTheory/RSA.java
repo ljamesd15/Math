840ccc29191cc5150b76bd0c592078709f537a4c
James Davidson <ljamesdavids@gmail.com>
2018-05-14 11:01:05 -0700

    Made a function which converts an int to binary

---
+++ src/numTheory/RSA.java
@@ -0,0 +1,48 @@
+package numTheory;
+
+public class RSA {
+
+	public static void main(String[] args) {
+		int[] arr = binaryOf(5);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
+	
+	/**
+	 * Returns the binary representation of the integer n.
+	 */
+	public static int[] binaryOf(int n) {
+		int currentPowOf2 = 2;
+		int count  = 2;
+		
+		// While n is greater than or equal to the next power of 2.
+		while(n >= currentPowOf2 * 2) {
+			currentPowOf2 *= 2;
+			count++;
+		}
+		
+		// Make a powers of 2 array of length count and fill in values
+		int[] powerOf2 = new int[count];
+		powerOf2[0] = 1;
+		for (int i = 1; i < count; i++) {
+			powerOf2[i] = powerOf2[i - 1] * 2;
+		}
+		
+		// If n is bigger than our next biggest power of 2 then subtract that from n and 
+		// set the bit in the bitset
+		int[] result = new int[count];
+		for (int i = powerOf2.length - 1; i > -1; i--) {
+			if (n >= powerOf2[i]) {
+				n = n - powerOf2[i];
+				result[i] = 1;
+			}
+		}
+		
+		assert(n == 0);
+		return result;
+		
+		
+	}
+}
